e97efa13259a45f972e3829ad9505712e2da928e
nikitacsk <nikita.voronkin3@gmail.com>
2024-10-08 16:48:25 +0200

    SQlite3 to PostgreSQL

--- cvsite/settings.py
+++ cvsite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'cvsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
